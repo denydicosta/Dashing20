95d30f29560df6d74408fbf4b340debe5ce207b2
denydicosta <122220955+denydicosta@users.noreply.github.com>
2023-05-16 23:23:16 +0500

    Update burhan.py

--- burhan.py
+++ burhan.py
@@ -1329,7 +1329,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("https://github.com/denydicosta/trick/tree/main").text
+	r1=requests.get("https://github.com/denydicosta/trick/tree/main").
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
